dc4eac86418fb086302a931152ec460f60f7b789
LickoPraveen003 <licko.praveen@hexacorp.com>
2024-11-11 19:04:36 +0530

    routing added

--- src/app/features/website/header/header.component.ts
+++ src/app/features/website/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -16,4 +17,8 @@ export class HeaderComponent {
   ];
 
   selectedLanguage = 'en';
+  constructor(private router: Router) {}
+  bookCall() {
+    this.router.navigate(['/therapix/bookcall']);
+  }
 }
